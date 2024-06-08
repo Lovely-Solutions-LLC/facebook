4c31bb3d74e7174f2051f00029ea7aafaee84d35
Matt Linder <linder2015@outlook.com>
2024-06-08 16:29:33 -0400

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { FacebookEmbed } from "react-social-media-embed";
 import useMonday from "./useMonday";
 
 export default function App() {
-  const { url, width, height, theme } = useMonday();
+  const { url, width, height, theme, isViewer } = useMonday();
 
   if (!url.includes("facebook.com")) {
     return (
       <main className={`center ${theme}`}>
         <p>Please enter a valid URL</p>
+      </main>
+    );
+  }
+
+  if (isViewer) {
+    return (
+      <main className={`center ${theme}`}>
+        <p>As a viewer, you are unable to make changes</p>
       </main>
     );
   }
--- src/useMonday.tsx
+++ src/useMonday.tsx
@@ -8,15 +8,17 @@ const monday = mondaySdk();
 
 export default function useMonday() {
   const [url, setUrl] = useState("");
+  const [isViewer, setViewer] = useState(true);
   const [theme, setTheme] = useState("light");
   const [width, setWidth] = useState(300);
   const [height, setHeight] = useState(600);
 
   useEffect(() => {
-    monday.execute("valueCreatedForUser")
+    monday.execute("valueCreatedForUser");
     monday.listen("context", (res) => {
-      setTheme(res.data.theme)
-    })
+      setTheme(res.data.theme);
+      setViewer(res.data.user.isViewOnly);
+    });
 
     monday.listen("settings", (res) => {
       setUrl(res.data.url);
@@ -25,5 +27,5 @@ export default function useMonday() {
     });
   }, []);
 
-  return { url, width, height, theme };
+  return { url, width, height, theme, isViewer };
 }
