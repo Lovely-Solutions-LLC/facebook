18e9806d0e6e27c8d1bb84b2c4db7754698ba330
Matt Linder <linder2015@outlook.com>
2024-06-21 19:01:00 -0400

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export default function App() {
   if (!url.includes("facebook.com")) {
     return (
       <main
+        className={theme}
         style={{
           width: "100%",
           height: "100vh",
